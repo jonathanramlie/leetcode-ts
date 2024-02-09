ae525f47c970f58435ad10864daf1188d4852673
jonathanramlie <jonathanramlie@outlook.com>
2024-02-10 01:04:13 +0700

    chore: update solution #2287 unit test wording

--- leetcode-ts/__tests__/2287-rearrange-characters-to-make-target-string.spec.ts
+++ leetcode-ts/__tests__/2287-rearrange-characters-to-make-target-string.spec.ts
@@ -2,13 +2,16 @@ import rearrangeCharacters from "../2287-rearrange-characters-to-make-target-str
 
 jest.setTimeout(150);
 
-describe("Sum of Scores of Built Strings", () => {
+describe("Rearrange Characters to Make Target String", () => {
   it.each([
     /* test case: [string, result ]*/
     ["ilovecodingonleetcode", "code", 2],
     ["abcba", "abc", 1],
     ["abbaccaddaeea", "aaaaa", 1],
-  ])("should return correct total score", (string, target, result) => {
-    expect(rearrangeCharacters(string, target)).toEqual(result);
-  });
+  ])(
+    "should return correct total possible arrangement",
+    (string, target, result) => {
+      expect(rearrangeCharacters(string, target)).toEqual(result);
+    }
+  );
 });
