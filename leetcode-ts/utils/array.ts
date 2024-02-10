58d585d21c8da5d0b057c82c5199f468ef173ead
jonathanramlie <jonathanramlie@outlook.com>
2024-02-10 21:16:05 +0700

    chore: add array helper for binary search

--- leetcode-ts/utils/array.ts
+++ leetcode-ts/utils/array.ts
@@ -5,3 +5,53 @@ export const swap = <T>(arr: T[], from: number, to: number): T[] => {
 
   return arr;
 };
+
+export const binarySearchArray = <T>(arr: T[], target: T): number => {
+  let left = 0;
+  let right = arr.length;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+export const binarySearchLowerBound = <T>(arr: T[], target: T): number => {
+  let lowerBound = 0;
+  let upperBound = arr.length;
+
+  while (lowerBound < upperBound) {
+    const mid = Math.floor((lowerBound + upperBound) / 2);
+
+    if (arr[mid] < target) {
+      lowerBound = mid + 1;
+    } else {
+      upperBound = mid;
+    }
+  }
+  return lowerBound;
+};
+
+export const binarySearchUpperBound = <T>(arr: T[], target: T): number => {
+  let lowerBound = 0;
+  let upperBound = arr.length;
+
+  while (lowerBound < upperBound) {
+    const mid = Math.floor((lowerBound + upperBound) / 2);
+
+    if (arr[mid] <= target) {
+      lowerBound = mid + 1;
+    } else {
+      upperBound = mid;
+    }
+  }
+
+  return upperBound;
+};
